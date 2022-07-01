234253aca7b9a4c9c9946683b7019e45d117e76c
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-01 20:25:06 -0300

    feat: add intial header structure

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <input type='text' placeholder='search user...'/>
+      <button type='submit'>Search</button>
+    </div>
+  )
+}
+
+export default Header;
