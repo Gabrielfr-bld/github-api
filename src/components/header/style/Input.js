c255525be1f78e67fcc16dbd53780be63428119c
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-01 20:47:05 -0300

    feat: implements style for input

---
+++ src/components/header/style/Input.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  border: 1px solid #000;
+  border-radius: 5px;
+  width: 100%;
+  height: 2rem;
+  padding: 1.5rem;
+  font-size: 1.2rem;
+  font-weight: 400;
+`;
