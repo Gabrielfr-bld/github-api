9c41cf04da80debb888bfa6fe41bf12d9a4809f4
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-01 19:15:36 -0300

    Revert "fix: rename function getAnyRepos in import/export"
    This reverts commit fd918625306f27528d9b7d80f4181e78d1f0618b.

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,9 @@
 import response from "./api/baseUrl";
 import getUser from "./api/getUser";
-import getAnyRepos from "./api/getRepos";
+import getRepos from "./api/getRepos";
 
 export {
   response,
   getUser,
-  getAnyRepos,
+  getRepos,
 }
