fd918625306f27528d9b7d80f4181e78d1f0618b
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-01 19:12:27 -0300

    fix: rename function getAnyRepos in import/export

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,9 @@
 import response from "./api/baseUrl";
 import getUser from "./api/getUser";
-import getRepos from "./api/getRepos";
+import getAnyRepos from "./api/getRepos";
 
 export {
   response,
   getUser,
-  getRepos,
+  getAnyRepos,
 }
