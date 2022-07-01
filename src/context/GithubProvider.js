64d31aeb8f6fd7c33621e1643a1164f5db3d1bde
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-01 19:40:09 -0300

    feat: implements state userData

--- src/context/GithubProvider.js
+++ src/context/GithubProvider.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState}from "react";
 import GithubContext from "./GithubContext";
 
 function GithubProvider({ children }) {
+
+  const [userData, setUserData] = useState({
+      id: 0,
+      avatar: '',
+      login: '',
+      name: '',
+      htmlUrl: '',
+      blog: '',
+      location: '',
+      followers: 0,
+      following: 0,
+      publicRepos: 0,
+      publicGists: 0,
+  })
+
   return (
     <GithubContext.Provider>
       {children}
