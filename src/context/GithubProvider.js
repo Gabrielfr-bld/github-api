7abcf0aaacf6f278fdc00842558918389a73761e
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-01 19:16:46 -0300

    feat: implements provider in app

--- src/context/GithubProvider.js
+++ src/context/GithubProvider.js
@@ -1,22 +1,7 @@
-import React, {useState} from "react";
+import React from "react";
 import GithubContext from "./GithubContext";
 
 function GithubProvider({ children }) {
-  const [user, setUser] = useState({
-    id: 0,
-    avatar: '',
-    login: '',
-    name: '',
-    htmlUrl: '',
-    blog: '',
-    company: '',
-    location: '',
-    followers: 0,
-    following: 0,
-    publicRepos: 0,
-    publicGists: 0,
-  });
-
   return (
     <GithubContext.Provider>
       {children}
