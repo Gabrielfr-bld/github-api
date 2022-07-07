679a6b03b8e3f0a1fe79de38b46a1ed2c6c9ae34
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-07 13:27:23 -0300

    feat: add component UserDetails to Profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import UserDetails from './userDetails/UserDetails';
 import UserImage from './UserImage';
 
 function Profile() {
   return (
     <div>
       <UserImage />
+      <UserDetails />
     </div>
   );
 }
