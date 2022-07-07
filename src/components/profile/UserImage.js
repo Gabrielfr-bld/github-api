9ee758d155c0d6d8eaf503ae77439d4e85759bbd
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-07 12:58:44 -0300

    feat: add style to UserImage

--- src/components/profile/UserImage.js
+++ src/components/profile/UserImage.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
 import GithubContext from '../../context/GithubContext'
+import { UserImageS } from './style/UserImageS'
 
 const UserImage = () => {
   const { userData } = useContext(GithubContext)
   return (
-    <img src={userData.avatar} alt={userData.login} />
+    <UserImageS src={userData.avatar} alt={userData.login} />
   )
 }
 export default UserImage;
