39a8e28181d9d2b4cd7fb096a5a05e454f5ba795
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-07 11:59:25 -0300

    feat: implements main component

---
+++ src/main/Main.js
@@ -0,0 +1,14 @@
+import { Header } from "../components";
+import { GlobalStyle } from "../global/GlobalStyle";
+
+
+function Main() {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
+  );
+}
+
+export default Main;
