44ac9f75832a52c4d672fb885009c25b787f6823
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-07 12:53:42 -0300

    feat: implements component Profile

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserImage from './UserImage';
+
+const Profile = () => {
+  return (
+    <div>
+      <UserImage />
+    </div>
+  );
+}
+
+export default Profile;
