41560b409e2063244c0b4a3b1415ee9d84975076
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 19:34:32 -0300

    feat: implements styledcomponent WrapperTabs

---
+++ src/components/repositories/style/WrapperTabs.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Tabs } from 'react-tabs';
+
+export const WrapperTabs = styled(Tabs)`
+  font-size: 1.2rem;
+  width: 100%;
+  margin-top: 1.2rem;
+`;
