5237c479feaf6c0d4db4105cc09c8f53e7a4f3f4
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 20:23:29 -0300

    feat: index to import/export styledcomponents

---
+++ src/components/repositories/style/index.js
@@ -0,0 +1,12 @@
+import { WrapperList } from "./RespositoryStyle/WrapperList";
+import { WrapperTabList } from "./RespositoryStyle/WrapperTabList";
+import { WrapperTab } from "./RespositoryStyle/WrapperTab";
+import { WrapperTabPanel } from "./RespositoryStyle/WrapperTabPanel";
+import { WrapperTabs } from "./RespositoryStyle/WrapperTabs";
+export {
+  WrapperList,
+  WrapperTabList,
+  WrapperTab,
+  WrapperTabPanel,
+  WrapperTabs
+};
