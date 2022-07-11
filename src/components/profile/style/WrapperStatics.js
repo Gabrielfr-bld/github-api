4c21bd01f21dfa5c334a7181a95dc881fb09882d
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 18:04:03 -0300

    feat: implements styled WrapperStatics

---
+++ src/components/profile/style/WrapperStatics.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const WrapperStatics = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 0.5rem;
+  div {
+    margin-top: 2.5rem;
+    margin-right: 1rem;
+    text-align: center;
+  }
+  h4 {
+    font-size: 1rem;
+    font-weight: bold;
+  }
+`;
