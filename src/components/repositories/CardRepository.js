81d5d25232f8382f2a92bc1a106f92739fee07ca
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 20:39:09 -0300

    feat: add WrapperTitle in CardRepository

--- src/components/repositories/CardRepository.js
+++ src/components/repositories/CardRepository.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { WrapperCard } from './style';
+import { WrapperCard, WrapperTitle } from './style';
 
 function CardRepository({ name, linkRepo, fullName }) {
   return (
     <WrapperCard>
-      <h1>{name}</h1>
+      <WrapperTitle>{name}</WrapperTitle>
       <h3>Full Name: </h3>
       <a href={linkRepo} target='_blank' rel='noreferrer'>{fullName}</a>
     </WrapperCard>
