1db03633e1f1cb784491c70766718959c099a972
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 15:31:48 -0300

    feat: add component UserInfo in UserDetails

--- src/components/profile/userDetails/UserDetails.js
+++ src/components/profile/userDetails/UserDetails.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import UserName from './UserName'
-
+import UserInfo from './UserInfo'
 
 function UserDetails() {
   return (
     <>
       <UserName />
+      <UserInfo />
     </>
   )
 }
