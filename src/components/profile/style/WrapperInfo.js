27fdba787738ce24e37f15ab5a2f4d83f22847d9
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 16:39:45 -0300

    feat: implements styled-component WrapperInfo

---
+++ src/components/profile/style/WrapperInfo.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const WrapperInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  margin-left: 1.2rem;
+  height: 30vh;
+`
