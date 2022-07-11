d7dc76abcd695e0e68a7c397d2024d100814c8da
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 20:31:40 -0300

    feat: implements styledcomponent WrapperCard

---
+++ src/components/repositories/style/CardRespositorStyle.js/WrapperCard.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const WrapperCard = styled.div`
+  padding: 1.5rem;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  margin: 1.5rem 1.5rem;
+  width: 20%;
+  height: 15vh;
+  align-content: center;
+`;
