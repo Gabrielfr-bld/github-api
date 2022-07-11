32bfdce855553243770f94bd0b7961d34f8b0ca2
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 18:42:54 -0300

    feat: implements component Repository

---
+++ src/components/repositories/Repository.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { GithubContext } from '../../context/GithubContext';
+import { response } from '../../services';
+
+function Repository() {
+  const { userData } = useContext(GithubContext);
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const getRepos = async (username, route) => {
+      const repos = await response.get(`users/${username}/${route}`)
+        .then(({ data }) => {
+          setRepositories(data);
+        });
+      return repos;
+    }
+    getRepos(`${userData.login}`, 'repos');
+    getRepos(`${userData.login}`, 'starred');
+  }, [repositories, userData.login])
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default Repository;
