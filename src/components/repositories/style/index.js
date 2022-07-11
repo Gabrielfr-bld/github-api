acc905976f20a35b48e20ff06208a4cef22e0fef
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 20:32:14 -0300

    feat: import/export WrapperCard in index

--- src/components/repositories/style/index.js
+++ src/components/repositories/style/index.js
@@ -3,10 +3,12 @@ import { WrapperTabList } from "./RespositoryStyle/WrapperTabList";
 import { WrapperTab } from "./RespositoryStyle/WrapperTab";
 import { WrapperTabPanel } from "./RespositoryStyle/WrapperTabPanel";
 import { WrapperTabs } from "./RespositoryStyle/WrapperTabs";
+import { WrapperCard } from "./CardRespositorStyle.js/WrapperCard";
 export {
   WrapperList,
   WrapperTabList,
   WrapperTab,
   WrapperTabPanel,
-  WrapperTabs
+  WrapperTabs,
+  WrapperCard
 };
