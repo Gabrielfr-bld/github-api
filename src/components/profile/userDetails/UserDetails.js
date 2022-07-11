6e69fc3a7246c01e62b653ebe9bc0d3da3e610bc
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 15:58:30 -0300

    feat: add UserStatics to UserDetails

--- src/components/profile/userDetails/UserDetails.js
+++ src/components/profile/userDetails/UserDetails.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import UserName from './UserName'
 import UserInfo from './UserInfo'
+import UserStatics from './UserStatics'
+import GithubContext from '../../../context/GithubContext'
 
 function UserDetails() {
+  const { userData } = useContext(GithubContext)
   return (
     <>
       <UserName />
       <UserInfo />
+      <UserStatics title="Followers" count={userData.followers} />
+      <UserStatics title="Followings" count={userData.following} />
+      <UserStatics title="Gists" count={userData.publicGists} />
+      <UserStatics title="Repos" count={userData.publicRepos} />
     </>
   )
 }
