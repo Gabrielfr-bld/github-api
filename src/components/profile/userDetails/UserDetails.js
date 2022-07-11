24dedc02f67c4fa3a86364cf8afdef5b94e7a595
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 16:40:27 -0300

    feat: add styleds components in UserDetails

--- src/components/profile/userDetails/UserDetails.js
+++ src/components/profile/userDetails/UserDetails.js
@@ -4,19 +4,23 @@ import UserInfo from './UserInfo'
 import UserStatics from './UserStatics'
 import UserImage from './UserImage'
 import GithubContext from '../../../context/GithubContext'
+import { WrapperInfo } from '../style/WrapperInfo'
+import { Wrapper } from '../style/Wrapper'
 
 function UserDetails() {
   const { userData } = useContext(GithubContext)
   return (
-    <>
+    <Wrapper>
       <UserImage avatar={userData.avatar} name={userData.name} />
-      <UserName name={userData.name} />
-      <UserInfo htmlUrl={userData.htmlUrl} login={userData.login} company={userData.company} location={userData.location} blog={userData.blog} />
+      <WrapperInfo>
+        <UserName name={userData.name} />
+        <UserInfo htmlUrl={userData.htmlUrl} login={userData.login} company={userData.company} location={userData.location} blog={userData.blog} />
+      </WrapperInfo>
       <UserStatics title="Followers" count={userData.followers} />
       <UserStatics title="Followings" count={userData.following} />
       <UserStatics title="Gists" count={userData.publicGists} />
       <UserStatics title="Repos" count={userData.publicRepos} />
-    </>
+    </Wrapper>
   )
 }
 export default UserDetails;
