9d3d1fab9a15d64cec4cd68d4c438fb69c9b2e14
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 18:04:32 -0300

    feat: add styledcomponent WrapperStatic

--- src/components/profile/userDetails/UserDetails.js
+++ src/components/profile/userDetails/UserDetails.js
@@ -6,6 +6,7 @@ import UserImage from './UserImage'
 import GithubContext from '../../../context/GithubContext'
 import { WrapperInfo } from '../style/WrapperInfo'
 import { Wrapper } from '../style/Wrapper'
+import { WrapperStatics } from '../style/WrapperStatics'
 
 function UserDetails() {
   const { userData } = useContext(GithubContext)
@@ -15,11 +16,13 @@ function UserDetails() {
       <WrapperInfo>
         <UserName name={userData.name} />
         <UserInfo htmlUrl={userData.htmlUrl} login={userData.login} company={userData.company} location={userData.location} blog={userData.blog} />
+        <WrapperStatics>
+          <UserStatics title="Followers" count={userData.followers} />
+          <UserStatics title="Followings" count={userData.following} />
+          <UserStatics title="Gists" count={userData.publicGists} />
+          <UserStatics title="Repos" count={userData.publicRepos} />
+        </WrapperStatics>
       </WrapperInfo>
-      <UserStatics title="Followers" count={userData.followers} />
-      <UserStatics title="Followings" count={userData.following} />
-      <UserStatics title="Gists" count={userData.publicGists} />
-      <UserStatics title="Repos" count={userData.publicRepos} />
     </Wrapper>
   )
 }
