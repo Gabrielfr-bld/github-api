b38e40ba48306b25083fb23655c259ff6c04e958
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 16:18:22 -0300

    refactor: remove component UserImage

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import UserDetails from './userDetails/UserDetails';
-import UserImage from './UserImage';
 
 function Profile() {
   return (
     <div>
-      <UserImage />
       <UserDetails />
     </div>
   );
