f45c257ac6b94106e4265a6956cd2e43d61424e2
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 15:39:11 -0300

    feat: add company, location and blog in UserInfo

--- src/components/profile/userDetails/UserInfo.js
+++ src/components/profile/userDetails/UserInfo.js
@@ -5,11 +5,17 @@ import GithubContext from '../../../context/GithubContext'
 function UserInfo() {
   const { userData } = useContext(GithubContext)
   return (
-    <>
+    <div>
       <a href={`${userData.htmlUrl}`}>
         {userData.login}
       </a>
-    </>
+      <div>
+        <h2>Company: {userData.company}</h2>
+        <h2>Location: {userData.location}</h2>
+        <h2>Blog:</h2>
+        <a href={`${userData.blog}`}>{userData.blog}</a>
+      </div>
+    </div>
 
   )
 }
