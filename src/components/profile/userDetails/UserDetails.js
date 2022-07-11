3b5670291726f1dc84862029d0d8b76b576d3f4b
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 16:19:32 -0300

    feat: add UserImage in UserDetails

--- src/components/profile/userDetails/UserDetails.js
+++ src/components/profile/userDetails/UserDetails.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import UserName from './UserName'
 import UserInfo from './UserInfo'
 import UserStatics from './UserStatics'
+import UserImage from './UserImage'
 import GithubContext from '../../../context/GithubContext'
 
 function UserDetails() {
   const { userData } = useContext(GithubContext)
   return (
     <>
+      <UserImage avatar={userData.avatar} name={userData.name} />
       <UserName name={userData.name} />
       <UserInfo htmlUrl={userData.htmlUrl} login={userData.login} company={userData.company} location={userData.location} blog={userData.blog} />
       <UserStatics title="Followers" count={userData.followers} />
