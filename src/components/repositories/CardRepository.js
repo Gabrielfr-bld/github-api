3fe4c70be113dab8c97f80b8c8286695ac8c8188
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-11 21:07:01 -0300

    feat: add Wrapper link in CarRepository

--- src/components/repositories/CardRepository.js
+++ src/components/repositories/CardRepository.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { WrapperCard, WrapperTitle, WrapperFullName } from './style';
+import { WrapperCard, WrapperTitle, WrapperFullName, WrapperLink } from './style';
 
 function CardRepository({ name, linkRepo, fullName }) {
   return (
     <WrapperCard>
       <WrapperTitle>{name}</WrapperTitle>
       <WrapperFullName>Full Name: </WrapperFullName>
-      <a href={linkRepo} target='_blank' rel='noreferrer'>{fullName}</a>
+      <WrapperLink href={linkRepo} target='_blank' rel='noreferrer'>{fullName}</WrapperLink>
     </WrapperCard>
   );
 }
