1f40a68102c9dd18d045cfb59206ce9dc2372707
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-12 10:23:04 -0300

    feat: import/export component NoSearch

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Profile from "./profile/Profile";
 import Layout from "./layout/Layout";
 import Repository from "./repositories/Repository";
 import Loading from "./loading/Loading";
+import NoSearch from "./noSearch/NoSearch";
 
-export { Profile, Layout, Repository, Loading };
+export { Profile, Layout, Repository, Loading, NoSearch };
