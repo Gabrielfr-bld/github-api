d9896d0af73852073ed7a7d5bc680b181e1735c6
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-12 09:59:01 -0300

    feat: add styled WrapperNoSearch to NoSearch

--- src/components/noSearch/NoSearch.js
+++ src/components/noSearch/NoSearch.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { WrapperNoSearch } from './style/WrapperNoSearch'
 
 function NoSearch() {
   return (
-    <>
+    <WrapperNoSearch>
       <h3>Digite um nome válido de usuário</h3>
-    </>
+    </WrapperNoSearch>
   )
 }
 
