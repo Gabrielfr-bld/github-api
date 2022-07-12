5c00d53df5bbb384b8141556ed34a4e13b360c6a
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-12 10:19:11 -0300

    feat: import state hasUser to Main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,8 +1,12 @@
+import React, { useContext } from "react";
 import { Layout, Profile, Repository } from "../components";
 import { GlobalStyle } from "../global/GlobalStyle";
-
+import GithubContext from "../context/GithubContext";
 
 function Main() {
+
+  const { hasUser } = useContext(GithubContext);
+
   return (
     <>
       <GlobalStyle />
