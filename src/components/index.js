29ef51771796a0336b94a1606d5122ab3b66ea4e
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-12 09:44:15 -0300

    feat: import/export component Loading

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Profile from "./profile/Profile";
 import Layout from "./layout/Layout";
 import Repository from "./repositories/Repository";
+import Loading from "./loading/Loading";
 
-export { Profile, Layout, Repository };
+export { Profile, Layout, Repository, Loading };
