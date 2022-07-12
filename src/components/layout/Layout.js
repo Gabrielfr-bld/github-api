7433260a8a852ee8067fee5688d89965539445ee
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-12 11:11:46 -0300

    feat: add WrapperLayout to Layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../header/Header';
+import { WrapperLayout } from './style/WrapperLayout';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <WrapperLayout>
       <Header />
       {children}
-    </>
+    </WrapperLayout>
   );
 }
 
