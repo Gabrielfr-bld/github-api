de5085c67be231063f58974386862b53d797cdb1
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-12 11:03:58 -0300

    refactor: styled component Input

--- src/components/header/style/Input.js
+++ src/components/header/style/Input.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Input = styled.input`
-  border: 1px solid #000;
+  border: 1px solid #fff;
   border-radius: 5px;
   width: 100%;
   height: 2rem;
   padding: 1.5rem;
   font-size: 1.2rem;
   font-weight: 400;
+  color: #fff;
 `;
